a3c8c585075c97d8da41460591fb60b3a99b0e11
Huaping Gu <humphrey.gu@gmail.com>
2014-07-16 16:54:49 -0700

    ebay.js

---
+++ ebay.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+var url = require('url');
+var app     = express();
+
+
+app.get('/ebay', function(req, res){
+	var reqUrl = url.parse(req.url,true);
+	var query = reqUrl.query;
+	var id = query.id;
+	var comingUrl = "http://www.ebay.com/itm/"+id;
+
+	request(comingUrl, function(error, response, html){
+		if(!error){
+			var $ = cheerio.load(html);
+
+			var title, release, rating;
+			var json = { title : "", links:[]};
+
+			$('.it-ttl').filter(function(){
+		        var data = $(this);
+		        title = data.text();            
+		        json.title = title;
+      
+	        })
+		}
+
+      
+      links = $('a'); //jquery get all hyperlinks
+  	  $(links).each(function(i, link){
+      		console.log($(link).attr('href'));
+      		var linkUrl = $(link).attr('href');
+      		if(linkUrl && (linkUrl.indexOf('http')>-1)){
+      			json.links.push({"name":$(link).text(), "URL": $(link).attr('href')});
+      		}
+  	  });
+
+
+      res.send(JSON.stringify(json, null, 4));
+
+
+
+        // Finally, we'll just send out a message to the browser reminding you that this app does not have a UI.
+       // res.send('Check your console!')
+	})
+})
+
+app.listen('8081')
+console.log('Magic happens on port 8081');
+exports = module.exports = app;
